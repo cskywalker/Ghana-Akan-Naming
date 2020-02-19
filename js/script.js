76bd8e778f4838ea3f8ab91c472b370e5fb149e3
cskywalker <wcndirangu@gmail.com>
2020-02-19 10:17:13 +0300

    Debug js function to take input and give feedback

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ function getAkanName () {
   var month = document.getElementById("month").value;
   var MM = parseInt(month);
   var year = document.getElementById("year").value;
-  var YY = parseInt(year.substr(year.length - 2););
-  var num = (parseInt(year);) / 100;
-  var CC = (num.toPrecision(2);) + 1;
+  // var YY = parseInt(year.substr(year.length - 2));
+  var YY = parseInt(year);
+  //var num = (parseInt(year)) / 100;
+  //var CC = (num.toPrecision(2)) + 1;
+  var CC = parseInt(YY - 1) / 100 + 1;
   var dayOfTheWeek = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
